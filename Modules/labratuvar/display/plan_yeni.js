ee490a39d052da34b13ec6083db2caa0a8c0104c
Emre Yilmaz <emre.yilmaz@hotmail.co.uk>
2022-06-21 10:29:24 +0300

    hicre süresler,e

--- Modules/labratuvar/display/plan_yeni.js
+++ Modules/labratuvar/display/plan_yeni.js
@@ -216,7 +216,7 @@ function getLData() {
         dataSource: data,
         views: ["timelineDay", {
             type: "day",
-            //cellDuration: 30,
+            cellDuration: 30,
             timeCellTemplate: function (data, index, element) {
                 element.text(data.text)
                     .css('color', 'green')
